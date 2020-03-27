a7bbcf4708558ad2958d31e8aed01b7edf9f96a7
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-27 20:17:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,7 +31,7 @@ def list_of_directors(source)
     source_index += 1
   end
   return result 
-end #returns an array of string ["Spielberg", "Ford"]
+end #returns an array of strings ["Spielberg", "Ford"]
 
 def total_gross(source)
   # Write this implementation
