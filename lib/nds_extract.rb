a102641bb45975612c15f3ad4426ea6549ae1356
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-27 20:09:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,8 @@ def total_gross(source)
     running_total += directors_totals(dir_array[dir_ind])
     dir_ind += 1
   end
-  puts ">>>" + running_total
+  puts ">>>" 
+  puts running_total
   return running_total  
 end
 
