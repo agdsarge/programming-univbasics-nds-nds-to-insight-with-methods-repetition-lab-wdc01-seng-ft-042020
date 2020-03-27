44d2c52ab8836b1329227985760db22a886ec43e
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-27 20:00:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
-  puts source
   result = {}
   director_index = 0
   while director_index < source.size do
@@ -27,6 +26,7 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  puts source
 end
 
 def total_gross(source)
